8ed63cf7c1191e8b51e7a71b72de0a665c76129a
keshav-16 <keshav162001@gmail.com>
2022-02-22 16:11:07 +0530

    Linked list implementation added

---
+++ Data Structure/Linked List/Linkked List Implementation/LinkedList.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct node{
+    char data;
+    struct node* next;
+}node;
+
+node* inLast(node* head,char data){
+    node* x;
+    x = (struct node*)malloc(sizeof(struct node));
+    node* p = head;
+    while(p->next != NULL){
+        p = p->next;
+    }
+    x->next = p->next;
+    x->data = data;
+    p->next = x;
+    return x;
+}
+
+void delFirst(node* head){
+    node* ptr1 = head;
+    node* ptr2 = head->next;
+    if(ptr2 == NULL){
+        cout<<"Empty"<<endl;
+    }
+    else{
+    ptr1->next = ptr2->next;
+    cout<<ptr2->data<<endl;
+    free(ptr2);
+    
+    }
+}
+
+void display(node* head){
+    node* p = head->next;
+    while(p){
+        cout<<p->data<<endl;
+        p = p->next;
+    }
+}
+
+int main(){
+    node* head;
+    head = (node*)malloc(sizeof(node));
+    head->data = 'h';
+
+    int k=0;
+    char data;
+    
+
+    while(k != 4){
+        cin>>k;
+
+        if(k == 1){
+            cin>>data;
+            inLast(head,data);
+        }
+        else if(k == 2){
+            delFirst(head);
+        }
+        else if(k == 3){
+            display(head);
+        }
+        else{
+            exit(0);
+        }
+    }
+    return 0;
+}
